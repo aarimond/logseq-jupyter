d98a0c761b24fce714e8dab5a152028692553f03
aarimond <alexander.arimond@sociovestix.com>
2022-12-11 21:04:07 +0100

    basic execution of python code blocks in logseq with jupyter

--- index.ts
+++ index.ts
@@ -1,21 +1,101 @@
 import "@logseq/libs";
 
+import { KernelManager, KernelMessage } from '@jupyterlab/services';
+import { ServerConnection } from "@jupyterlab/services";
+import { PageConfig } from '@jupyterlab/coreutils';
 
-function main () {
 
-  logseq.Editor.registerSlashCommand(
-    'jupyter',
-    async () => {
-      const { content, uuid } = await logseq.Editor.getCurrentBlock()
+function extractCodeFromBlockContent(content: string): string | undefined {
+  // TODO be less rigid on where to place "python"
+  const codeRegex = /```\s+python\n(?<code>[\s\S]*?)\n```/m;
+  return content.match(codeRegex)?.groups?.code;
+}
 
-      logseq.App.showMsg(`
-        [:div.p-2
-          [:h1 "#${uuid}"]
-          [:h2.text-xl ${content}]]
-      `)
 
+function showJupyterError(msg: string, status: 'warning' | 'error', timeout?: number) {
+  logseq.UI.showMsg(`
+    [:div.p-2 
+      [:h2.text-xl "logseq-jupyter"]
+      [:div.p-2 "${msg}"]]
+  `, status, { timeout: timeout ? timeout : 5000 });
+}
+
+
+async function runJupyterCode(baseUrl: string, token: string, code: string) {
+  PageConfig.setOption('baseUrl', baseUrl);
+  PageConfig.setOption('token', token);
+
+  const serverSettings = ServerConnection.makeSettings({
+    appendToken: true
+  });
+
+  const kernelManager = new KernelManager({ serverSettings });
+  const kernel = await kernelManager.startNew({ name: 'python' });
+
+  const future = kernel.requestExecute({ code });
+
+  var output;
+  future.onIOPub = (msg: KernelMessage.IIOPubMessage) => {
+    console.log(msg);
+    if (KernelMessage.isExecuteResultMsg(msg)) {
+      output = msg.content.data['text/plain']
     }
-  )
+  };
+
+  await future.done;
+  await kernel.shutdown();
+
+  return output;
+}
+
+
+async function runJupyterCommand() {
+  const block = await logseq.Editor.getCurrentBlock();
+
+  if (block === null) {
+    showJupyterError('No Block selected.', 'warning');
+    return;
+  }
+
+  const code = extractCodeFromBlockContent(block.content);
+  if (code === undefined) {
+    showJupyterError('Not able to select code', 'warning');
+    return;
+  }
+
+  logseq.Editor.getBlockProperty(block.uuid, 'jupyter').then((jupyterProperty: string | null) => {
+
+    if (!jupyterProperty) {
+      showJupyterError("Not able to read property 'jupyter' from selected block.", 'warning');
+      return;
+    }
+
+    const url = new URL(jupyterProperty);
+    const baseUrl = url.origin;  // e.g. "http://localhost:8888"
+    const token = url.searchParams.get('token');
+
+    if (!(baseUrl && token)) {
+      showJupyterError('Not able to read baseUrl and token from block properties.', 'error');
+      return;
+    }
+
+    runJupyterCode(baseUrl, token, code).then(output => {
+      logseq.Editor.insertBlock(
+        block.uuid,
+        "``` shell\n#Output:\n" + output + "\n```\n", { before: false }
+      ).then(
+        () => logseq.Editor.exitEditingMode()
+      );
+    });
+
+  }).catch(err => {
+    console.log(err);
+    showJupyterError('Problems reading properties from block.', 'error');
+  })
+}
+
+async function main(): Promise<void> {
+  logseq.Editor.registerSlashCommand('jupyter', runJupyterCommand);
 }
 
 // bootstrap
